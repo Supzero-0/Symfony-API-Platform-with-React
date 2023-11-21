384cfcac093f62145147ce4a14e1fa417512cddd
Supzero-0 <mat.lecanu@gmail.com>
2023-11-21 15:36:14 +0100

    Modification of list function in BlogController

--- server/src/Controller/BlogController.php
+++ server/src/Controller/BlogController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/blog', name: 'blog_')]
@@ -27,21 +28,30 @@ class BlogController extends AbstractController
         ],
     ];
 
-    #[Route('/', name: 'list')]
-    public function list(): JsonResponse
+    #[Route('/{page}', name: 'list', defaults: ['page' => 1], requirements: ['page' => '\d+'])]
+    public function list($page = 1, Request $request): JsonResponse
     {
-        return new JsonResponse(self::POSTS);
+        $limit = $request->get('limit', 10);
+        return $this->json(
+            [
+                'page' => $page,
+                'limit' => $limit,
+                'data' => array_map(function ($item) {
+                    return $this->generateUrl('blog_show_id', ['id' => $item['id']]);
+                }, self::POSTS),
+            ]
+        );
     }
 
-    #[Route('/{id}', name: 'show', requirements: ['id' => '\d+'])]
+    #[Route('/show/{id}', name: 'show_id', requirements: ['id' => '\d+'])]
     public function show($id): JsonResponse
     {
-        return new JsonResponse(self::POSTS[array_search($id, array_column(self::POSTS, 'id'))]);
+        return $this->json(self::POSTS[array_search($id, array_column(self::POSTS, 'id'))]);
     }
 
-    #[Route('/{slug}', name: 'show_slug')]
+    #[Route('/show/{slug}', name: 'show_slug')]
     public function showSlug($slug): JsonResponse
     {
-        return new JsonResponse(self::POSTS[array_search($slug, array_column(self::POSTS, 'slug'))]);
+        return $this->json(self::POSTS[array_search($slug, array_column(self::POSTS, 'slug'))]);
     }
 }
